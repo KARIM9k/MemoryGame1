e18ef716d2c110b3b55c4dde2f24711c4e0aab97
KARIM9K <kareem081081@gmail.com>
2026-09-19 14:00:42 +0300

    klk

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -54,7 +54,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(770, 473);
+            this.button2.Location = new System.Drawing.Point(316, 186);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(84, 29);
