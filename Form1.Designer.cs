ced52d6852f06ff98f6cde39497f4e5f0128f7bc
KARIM9K <kareem081081@gmail.com>
2026-09-19 16:38:40 +0300

    a

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -136,7 +136,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(98, 54);
+            this.label1.Location = new System.Drawing.Point(169, 160);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(358, 69);
             this.label1.TabIndex = 0;
