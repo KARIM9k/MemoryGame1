802d0838d8f1290cd9ae263cb1dab4fef8792cad
Ahmad Malak <ahmad3511malak@gmail.com>
2026-09-19 16:36:11 +0300

    Update

--- Form1.cs
+++ Form1.cs
@@ -10,13 +10,39 @@ using System.Windows.Forms;
 
 namespace MemoryGame1
 {
+
+    public enum enLevel {enEasy=0,enMid=1,enHard=2 };
+    public enum enGender {Male =0, Female=1 }; 
+
+    public struct stGameInfo 
+    {
+       public enLevel Level;
+       public bool TowPlayers;
+       public int NumberOfRounds;
+       public int TimePerRound;
+       public string NamePlayer1;
+       public string NamePlayer2;
+       public string GenderPlayer1;
+       public string GenderPlayer2;
+       public string Winner;
+    }
+
     public partial class Form1 : Form
     {
+        stGameInfo GameInfo; 
+       
         public Form1()
         {
             InitializeComponent();
+            switch (GameInfo.Level) { }
+            
         }
 
-      
+
+
+
+
+
+
     }
 }
